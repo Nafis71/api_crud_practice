48f808ca6b4d0d34f661930c9f3491d752f1f842
Nafis71 <nhtonmoy2@gmail.com>
2024-05-22 23:35:59 +0600

    minor changes

--- lib/views/homeScreen/home_screen.dart
+++ lib/views/homeScreen/home_screen.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:api_crud_practice/controllers/data_controller.dart';
 import 'package:api_crud_practice/controllers/data_repository.dart';
 import 'package:api_crud_practice/models/product_model.dart';
@@ -11,12 +9,8 @@ import 'package:api_crud_practice/views/homeScreen/product_list_layout.dart';
 import 'package:api_crud_practice/views/widgets/app_alert_dialog.dart';
 import 'package:api_crud_practice/views/widgets/app_snackbar.dart';
 import 'package:api_crud_practice/views/widgets/app_toast.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
-import 'package:gap/gap.dart';
 import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
-import 'package:toastification/toastification.dart';
 import '../../utils/text_constants.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -44,11 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text(homeScreenAppbarTitle),
         actions: [
           IconButton(
-              onPressed: () {},
-              icon: const Icon(
-                Icons.more_vert,
-                size: 30,
-              )),
+              onPressed: () {}, icon: const Icon(Icons.more_vert, size: 30)),
         ],
       ),
       body: OrientationBuilder(
@@ -66,7 +56,7 @@ class _HomeScreenState extends State<HomeScreen> {
                         productListNotifier.value = snapshot.data!;
                         return Visibility(
                           visible: snapshot.data!.isNotEmpty,
-                          replacement: NoProductData(onRefresh: () async{
+                          replacement: NoProductData(onRefresh: () async {
                             List<ProductModel> productList =
                                 await dataController.getProductData();
                             if (productList.isNotEmpty) {
@@ -81,9 +71,9 @@ class _HomeScreenState extends State<HomeScreen> {
                               Widget? child,
                             ) {
                               if (productList.isEmpty) {
-                                return NoProductData(onRefresh: () async{
+                                return NoProductData(onRefresh: () async {
                                   List<ProductModel> productList =
-                                  await dataController.getProductData();
+                                      await dataController.getProductData();
                                   if (productList.isNotEmpty) {
                                     setState(() {});
                                   }
@@ -181,8 +171,6 @@ class _HomeScreenState extends State<HomeScreen> {
 
   void postUpdateMessage(String message) {
     Future.delayed(const Duration(milliseconds: 900), () {
-      // ScaffoldMessenger.of(context).showSnackBar(
-      //     appSnackbar(message, appPrimaryColor));
       showToast(content: message, context: context);
     });
   }
