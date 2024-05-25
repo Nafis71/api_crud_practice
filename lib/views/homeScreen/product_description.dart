fa84281c2a484a6651cc308158fdc29f1a54551c
Nafis71 <nhtonmoy2@gmail.com>
2024-05-26 00:18:58 +0600

    changed lint version

--- lib/views/homeScreen/product_description.dart
+++ lib/views/homeScreen/product_description.dart
@@ -8,7 +8,14 @@ class ProductDescription extends StatelessWidget {
   final Color color;
   final double fontSize;
   final FontWeight fontWeight;
-  const ProductDescription({super.key, required this.text,this.maxLines = 1, required this.color, required this.fontSize, required this.fontWeight});
+
+  const ProductDescription(
+      {super.key,
+      required this.text,
+      this.maxLines = 1,
+      required this.color,
+      required this.fontSize,
+      required this.fontWeight});
 
   @override
   Widget build(BuildContext context) {
